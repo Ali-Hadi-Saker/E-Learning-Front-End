ff9861ec97e4bb4ec119c0c762129571860a351e
Ali-Hadi-Saker <alihady530@gmail.com>
2024-08-06 12:25:07 +0300

    implementing register form

--- e-learning-app/src/Pages/Register/index.jsx
+++ e-learning-app/src/Pages/Register/index.jsx
@@ -1,18 +1,36 @@
 import React from "react"; 
 import './style.css'
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 
 const Register = ()=>{
+    const [username, setUsername] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleChange = (e)=>{
+        console.log(e.target.value)
+    }
+    const handleSubmit = ()=> {console.log(submit)}
     return(
         <div className="flex column page center ">
             <h1>register page</h1>
             <div className="flex column center auth-box">
-                <input type="text" placeholder="username"/>
-                <input type="text" placeholder="email"/>
-                <input type="password" placeholder="password"/>
+                <input type="text" 
+                        placeholder="username"
+                        onChange={handleChange}
+                        value={username}/>
+                <input type="text" 
+                        placeholder="email"
+                        onChange={handleChange}
+                        value={email}/>
+                <input type="password" 
+                        placeholder="password"
+                        onChange={handleChange}
+                        value={password}/>
 
-                <button>Sign up</button>
+                <button onClick={handleSubmit}>Sign up</button>
                 <p className="login-link">already have an account{"  "}
                 <Link to="/login">login here</Link>
                 </p>
