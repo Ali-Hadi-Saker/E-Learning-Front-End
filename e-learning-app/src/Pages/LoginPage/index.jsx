7524aa94a5e91b3d14de1baeff0262ad8f7d67f1
Ali-Hadi-Saker <alihady530@gmail.com>
2024-08-06 13:38:22 +0300

    fetching login api

--- e-learning-app/src/Pages/LoginPage/index.jsx
+++ e-learning-app/src/Pages/LoginPage/index.jsx
@@ -1,18 +1,57 @@
 import React from "react"; 
 import './style.css'
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios"
 
 
 
 const Login = ()=>{
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleChange = (e)=>{
+        const {name, value} = e.target
+        if(name === 'email') setEmail(value)
+        if(name === 'password') setPassword(value)
+    }
+
+    const fetchUser = async (email, password)=>{
+        try {
+            const {data} = await axios.post('http://localhost:8080/users/login', {
+                email,
+                password
+            })
+            return data
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    const handleSubmit = async()=>{
+        try {
+            const data = await fetchUser(email, password)
+            console.log(data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
     return(
         <div className="flex column page center ">
             <h1>login page</h1>
             <div className="flex column center auth-box">
-                <input type="text" placeholder="email"/>
-                <input type="password" placeholder="password"/>
+            <input type="text" 
+                        name="email"
+                        placeholder="email"
+                        onChange={handleChange}
+                        value={email}/>
+                <input type="password" 
+                        name="password"
+                        placeholder="password"
+                        onChange={handleChange}
+                        value={password}
+                        />
 
-                <button>Login</button>
+                <button onClick={handleSubmit}>Login</button>
                 <p className="signup-link">Don t have an account?{"  "}
                 <Link to="/">Signup here</Link>
                 </p>
