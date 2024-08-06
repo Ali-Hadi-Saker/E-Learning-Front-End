4dd19302260d97b908cc799a673cc45e641ff16e
Ali-Hadi-Saker <alihady530@gmail.com>
2024-08-06 13:13:52 +0300

    fetching register api

--- e-learning-app/src/Pages/Register/index.jsx
+++ e-learning-app/src/Pages/Register/index.jsx
@@ -2,30 +2,57 @@ import React from "react";
 import './style.css'
 import { Link } from "react-router-dom";
 import { useState } from "react";
+import axios from 'axios'
 
 
 const Register = ()=>{
-    const [username, setUsername] = useState('')
+    const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
     const handleChange = (e)=>{
-        console.log(e.target.value)
+        const {name, value} = e.target
+        if(name === 'name') setName(value)
+        if(name === 'email') setEmail(value)
+        if(name === 'password') setPassword(value)
     }
-    const handleSubmit = ()=> {console.log(submit)}
+
+    const fetchUser = async (name, email, password)=>{
+        try{
+            const {data} = await axios.post('http://localhost:8080/users/register', {
+                name,
+                email,
+                password
+            })
+            return data
+        }catch(e){
+            console.log(e)
+        }
+    }
+    const handleSubmit = async()=> {
+        try{
+            const data = await fetchUser(name, email, password)
+            console.log(data)
+        }catch(e){
+            console.log(e)
+        }
+    }
     return(
         <div className="flex column page center ">
             <h1>register page</h1>
             <div className="flex column center auth-box">
                 <input type="text" 
-                        placeholder="username"
+                        name="name"
+                        placeholder="name"
                         onChange={handleChange}
-                        value={username}/>
+                        value={name}/>
                 <input type="text" 
+                        name="email"
                         placeholder="email"
                         onChange={handleChange}
                         value={email}/>
                 <input type="password" 
+                        name="password"
                         placeholder="password"
                         onChange={handleChange}
                         value={password}/>
